5e34b1a32977327d2a082967285f97f9ae2319a3
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-12-01 21:56:01 +0200

    Show items list

--- InAndOut/Controllers/ItemController.cs
+++ InAndOut/Controllers/ItemController.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using InAndOut.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace InAndOut.Controllers
 {
     public class ItemController : Controller
     {
+        private readonly ApplicationDbContext _dbContext;
+
+        public ItemController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var items = _dbContext.Items.AsNoTracking();
+
+            return View(items);
         }
     }
 }
